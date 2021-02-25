dba58195612e78370d825d9af85adca3948513f9
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2021-02-25 22:36:12 +0100

    poprawa bledu z wyswietlaniem notatki nowej i jej poprzednikow

--- Programowanie-w-Javascript/cw3-noteKeep/main.js
+++ Programowanie-w-Javascript/cw3-noteKeep/main.js
@@ -3,43 +3,61 @@ let notes=[];
 
 document.querySelector('#addBtn').addEventListener('click', onNewNote);
 
-// nowa notatka
-const note ={
-    title: 'notatka tytuł',
-    content: 'zawartość',
-    colour: 'black',
-    pinned: false,
-    createDate: new Date() 
-};
-
-// dodawanie notatek
-function onNewNote() {
-    const title = document.querySelector('#noteTitle').value;
-    const content = document.querySelector('#noteContent').value;
-    console.log(title,content);
-
-    note.content=content;
-    note.title=title;
-
-    notes.push(note);
-
-    showNotes();
-}
-
-// tablica zapisana w localStorage
-localStorage.setItem(localStorageKey, JSON.stringify(notes));
-
 // odczytanie tablicy notatek z localStorage
-const notesFromStorage = JSON.parse(localStorage.getItem(localStorageKey));
+let notesFromStorage = JSON.parse(localStorage.getItem(localStorageKey));
 notes = notesFromStorage.map(note => {
     note.createDate = new Date(note.createDate);
     return note;
 });
 
+// dodawanie notatek
+function onNewNote() {
+    let title = document.querySelector('#noteTitle').value;
+    let content = document.querySelector('#noteContent').value;
+    console.log(title,content);
+
+    if (notesFromStorage == null){
+        notes = [];
+    }else{
+        notes=notesFromStorage;
+    }
+
+    // nowa notatka
+    let note ={
+        title: 'notatka tytuł',
+        content: 'zawartość',
+        colour: 'black',
+        pinned: false,
+        createDate: new Date() 
+    };
+
+    note.title=title;
+    note.content=content;
+    note.createDate = new Date();
+
+    notes.push(note);
+    notes = localStorage.setItem(localStorageKey, JSON.stringify(notes));
+
+    title.value='';
+    content.value='';
+
+    showNotes();
+}
+
+
+
+
+
 function showNotes() {
+    
+    if (notesFromStorage == null){
+        notes = [];
+    }else{
+        notes=notesFromStorage;
+    }
+    let htmlNote='';
     notes.forEach(function (element,index) {
-        const main=document.querySelector('main');
-        const htmlNote =`
+        htmlNote +=`
         <section class="note">
         <h2>${element.title}</h2>
         <p>${element.content}</p>
@@ -50,7 +68,11 @@ function showNotes() {
         class="deleteNote">Delete</butto>
         </section>
     `;
-        main.innerHTML += htmlNote;
+        let main=document.querySelector('main');
+        if(notes.length!=0)
+            main.innerHTML = htmlNote;
+        else   main.innerHTML='biasdda';
     });
 }
 
+showNotes();

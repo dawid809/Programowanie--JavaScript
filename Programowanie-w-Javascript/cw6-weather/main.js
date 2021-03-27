f48996c89975de3b2bd8c24cd50e2d52e09b9f3c
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2021-03-27 11:10:37 +0100

    Pierwsze działania na LocalStorage

--- Programowanie-w-Javascript/cw6-weather/main.js
+++ Programowanie-w-Javascript/cw6-weather/main.js
@@ -9,6 +9,8 @@ const apiKey = 'c18e218d437295f57f6cf1900d5fe9cc';
 // Zdarzenie klika dla buttona
 addCityButton.addEventListener('click', GetDataFromAPI);
 
+const localStorageKey = 'weathers';
+let weatherArray = [];
 
 function GetDataFromAPI() {
     const cityInput = inputText.value;
@@ -17,9 +19,8 @@ function GetDataFromAPI() {
 
     // Otrzymanie danych z API (fetch)
     fetch(apiUrl)
-        .then(data =>{
-            return data.json();
-        }).then(showWeatherReport)
+        .then(data => data.json())
+        .then(showWeatherReport)
         // .catch(error => console.log('Błąd: ', error));
         .catch(error => console.log('Błąd: ', error)).innerHTML='Zła nazaww misata';
 }
@@ -34,16 +35,33 @@ function  showWeatherReport(data) {
     icon.src = iconUrl;
     console.log(iconUrl);
 
+    let weatherObj = {
+        name: data.name,
+        main : {
+            description: data.weather[0].description,
+            temperature: data.main.temp,
+            pressure: data.main.pressure,
+            humidity: data.main.humidity
+        }
+    };
+
+    weatherArray.push(weatherObj);
+    weatherArray = localStorage.setItem(localStorageKey, JSON.stringify(weatherObj));
     // Wyświetlenie wszystkich danych 
+    console.log(weatherObj);
+
     showData.innerHTML +=`
          <div class="weatherWrapper">
-            <div class="cityInfo">${data.name}, ${data.sys.country}</div>
-            <div class="description">${data.weather[0].description}</div>
-            <div class="temperature">${data.main.temp}°C</div>
-            <div class="icon"><img src= ${icon.src}></div>
-            <div class="pressure">Ciśnienie: ${data.main.pressure} hPA</div>
-            <div class="humidity">Wilgotność: ${data.main.humidity}%</div>
+            <div class="cityInfo">${weatherObj.name}</div>
+            <div class="description">${weatherObj.main.description}</div>
+            <div class="temperature">${weatherObj.main.temperature}°C</div>
+            <div class="pressure">Ciśnienie: ${weatherObj.main.pressure} hPA</div>
+            <div class="humidity">Wilgotność: ${weatherObj.main.humidity}%</div>
          </div>
       `; 
 }
 
+
+
+//weatherArray.push(weatherObj);
+console.log(weatherArray);
